e53fdd630edc7d8485035d17f8bbe73060b27cdd
Mateusz Haligowski <mhaligowski@gmail.com>
2016-11-24 15:28:15 -0800

    Handle request with Gorilla multiplexer

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package feedupdater
 
 import (
 	"net/http"
+	"github.com/gorilla/mux"
 	"fmt"
 )
 
 
-func init() {
-	http.HandleFunc("/items", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello!")
-	})
+func handleGetItems(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello!")
+}
 
+func init() {
+	r := mux.NewRouter();
+	r.HandleFunc("/items", handleGetItems)
+
+	http.Handle("/", r)
 }
