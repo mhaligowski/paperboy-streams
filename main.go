ea846274d85891ca1cdc4c1c31206f35eff1ab38
Mateusz Haligowski <mhaligowski@gmail.com>
2016-11-25 23:31:02 -0800

    Fetch the data from datastore

--- main.go
+++ main.go
@@ -1,14 +1,40 @@
-package feedupdater
+package streams
 
 import (
 	"net/http"
 	"github.com/gorilla/mux"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
 	"fmt"
+	"google.golang.org/appengine/log"
 )
 
+type StreamItem struct {
+	StreamItemId  string `datastore:"id"`;
+	UserId        string;
+	TargetId      string;
+	Title         string;
+	OrderSequence int64;
+}
 
 func handleGetItems(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello!")
+	ctx := appengine.NewContext(r)
+
+	query := datastore.NewQuery("StreamItem").Order("-order_sequence")
+
+	var items []StreamItem
+	_, err := query.GetAll(ctx, &items)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Errorf(ctx, "Error when querying from datastore", err)
+		return
+	}
+
+	for _, item := range items {
+		fmt.Fprintf(w, "%v\n", item.Title)
+	}
+
 }
 
 func init() {
