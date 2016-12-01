895859679a8a6bf9ca4060623b4f31c155915634
Mateusz Haligowski <mhaligowski@gmail.com>
2016-11-30 22:31:44 -0800

    Test for empty response

---
+++ server_test.go
@@ -0,0 +1,22 @@
+package streams
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+)
+
+func TestEmpty(t *testing.T) {
+	f := func(r *http.Request) ([]StreamItem, error) {
+		return make([]StreamItem, 0), nil
+	}
+
+	w := httptest.NewRecorder()
+	handleGetStreamItems(w, nil, f)
+
+	if w.Body.String() != "[]\n" {
+		t.Errorf("Expected [], got %q", w.Body.String())
+	}
+}
+
+
